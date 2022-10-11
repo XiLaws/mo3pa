18006464e70e26d8faf8f419c1015c23c2f659db
XiLaws <90407785+XiLaws@users.noreply.github.com>
2022-10-11 12:59:15 -0400

    Add files via upload

---
+++ 7.py
@@ -0,0 +1,6 @@
+things = list(['mozzarella', 'cinderella'])
+for i in things:
+    print(i.capitalize())
+for o in things:
+    print(o.upper())
+things
---
+++ 9.py
@@ -0,0 +1,13 @@
+def good():
+    return ['Harry', 'Ron', 'Hermione']
+
+
+print(good())
+limit = 10
+get_odds = (num for num in range(limit) if not num % 2 == 0)
+count = 0
+for num in get_odds:
+    if count == 2:
+        print(num)
+        break
+    count += 1
